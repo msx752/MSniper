a279a977914ea68825a902c71f61b31ef2f79822
MSX752 <msx752@users.noreply.github.com>
2016-09-25 11:49:26 +0300

    no message

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,20 +2,19 @@
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
 function gaa(){
-  ga('create', 'UA-74939359-3', {'allowAnchor': true});
-  ga('require', 'linkid');
-  ga('send', 'pageview', { 'page': location.pathname + location.search + location.hash});
+	gaa(location.pathname + location.search + location.hash);
 }
-
+function gaa(urlparm){
+	ga('create', 'UA-74939359-3', {'allowAnchor': true});
+	ga('require', 'linkid');
+	ga('send', 'pageview', { 'page': urlparm});
+	}
 $(document).ready(function () {
-		gaa();
-
+	gaa();
 		$('a').click(function(){
-			gaa();
+			gaa(location.pathname + this.innerText);
 		});
-
-		});
+});
 
  
